7299b30d295c028516f1e6dae39d9027529c2935
quocbahuynh <jbrteamwebsite@gmail.com>
2022-06-18 17:22:24 +0700

    env

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <GoogleReactLogin
-          client_id={
-            "107802859705-0pn75l7ihog8et2fe4s84kqnutpn6el5.apps.googleusercontent.com"
-          }
+          client_id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
           onSuccess={onSuccess}
           onFailure={onFailure}
           onTap={false}
